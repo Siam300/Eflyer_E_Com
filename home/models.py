1cca4bb9067a8149549770ee89694dd0d212a220
Siam300 <siamkhan8523@gmail.com>
2023-03-07 12:21:24 +0600

    ...

--- home/models.py
+++ home/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
 # Create your models here.
+class manTshirt(models.Model):
+    productName= models.CharField()
+    productPrice= models.FloatField()
+    productImage= models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        return "%s" %(self.productName)  (self.productPrice)
+    
+    class manShirt(models.Model):
+    productName= models.CharField()
+    productPrice= models.FloatField()
+    productImage= models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        return "%s" %(self.productName)  (self.productPrice)
+    
+    class womanScart(models.Model):
+    productName= models.CharField()
+    productPrice= models.FloatField()
+    productImage= models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        return "%s" %(self.productName)  (self.productPrice)
+    
+    class laptop(models.Model):
+    productName= models.CharField()
+    productPrice= models.FloatField()
+    productImage= models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        return "%s" %(self.productName)  (self.productPrice)
+    
+    class mobile(models.Model):
+    productName= models.CharField()
+    productPrice= models.FloatField()
+    productImage= models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        return "%s" %(self.productName)  (self.productPrice)
+    
+    class computers(models.Model):
+    productName= models.CharField()
+    productPrice= models.FloatField()
+    productImage= models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        return "%s" %(self.productName)  (self.productPrice)
+    
+    class jhumkas(models.Model):
+    productName= models.CharField()
+    productPrice= models.FloatField()
+    productImage= models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        return "%s" %(self.productName)  (self.productPrice)
+    
+    class necklaces(models.Model):
+    productName= models.CharField()
+    productPrice= models.FloatField()
+    productImage= models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        return "%s" %(self.productName)  (self.productPrice)
+    
+    class kangans(models.Model):
+    productName= models.CharField()
+    productPrice= models.FloatField()
+    productImage= models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        return "%s" %(self.productName)  (self.productPrice)
